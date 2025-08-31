859c019ebfcb34e121483e3e91be82b2a7ea0040
Udoka Uzor <udokauzor67@gmail.com>
2025-08-31 19:49:12 +0100

    Fix: Register Book model with filters and search

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,15 +1,8 @@
 from django.contrib import admin
 from .models import Book
 
+@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    # Show these fields as columns in the list view
-    list_display = ('title', 'author', 'publication_date')
-
-    # Add filters on the right-hand side
-    list_filter = ('author', 'publication_date')
-
-    # Add a search bar at the top
-    search_fields = ('title', 'author')
-
-# Register the Book model with this custom admin class
-admin.site.register(Book, BookAdmin)
+    list_display = ("title", "author", "publication_date")
+    search_fields = ("title", "author", "isbn")
+    list_filter = ("author", "publication_date", "language")
