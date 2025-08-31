3a3bda4a10ab82c96a701f1df1d232f389d864e3
Udoka Uzor <udokauzor67@gmail.com>
2025-08-31 19:28:30 +0100

    Fix: register Book model in admin with filters and search

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -3,7 +3,11 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+    publication_date = models.DateField()
+    isbn = models.CharField(max_length=13)
+    pages = models.IntegerField()
+    cover = models.CharField(max_length=100, default="Unknown")  # added default
+    language = models.CharField(max_length=50, default="English")  # safe default
 
     def __str__(self):
-        return f"{self.title} by {self.author} ({self.publication_year})"
+        return self.title
