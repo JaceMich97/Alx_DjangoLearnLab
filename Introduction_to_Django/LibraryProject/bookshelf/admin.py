d21ba14fd757d62483961f58bd9806960286d20f
Udoka Uzor <udokauzor67@gmail.com>
2025-09-09 18:51:06 +0100

    Add relationship_app and update settings and admin

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -3,6 +3,6 @@ from .models import Book
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ("title", "author", "publication_date")
+    list_display = ("title", "author", "publication_date", "isbn", "pages", "cover", "language")
     search_fields = ("title", "author", "isbn")
     list_filter = ("author", "publication_date", "language")
