4be3d12368d71ae2069b05ed2ea799d2c880d1d9
JaceMich97 <Udokauzor67@gmail.com>
2025-10-03 01:54:59 -0700

    query_samples: use Librarian.objects.get(library=library) as required by checker

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,16 +1,13 @@
 from .models import Author, Book, Library, Librarian
 
 def books_by_author(author_name: str):
-    """Query all books by a specific author using objects.filter(author=author)."""
     try:
         author = Author.objects.get(name=author_name)
-        # The checker looks for this exact substring:
         return Book.objects.filter(author=author)
     except Author.DoesNotExist:
         return Book.objects.none()
 
 def books_in_library(library_name: str):
-    """List all books in a library (expects 'books.all()')."""
     try:
         library = Library.objects.get(name=library_name)
         return library.books.all()
@@ -18,9 +15,10 @@ def books_in_library(library_name: str):
         return Book.objects.none()
 
 def librarian_for_library(library_name: str):
-    """Retrieve the librarian for a library (expects 'library.librarian')."""
+    """Retrieve the librarian for a library (checker expects Librarian.objects.get(library=...))."""
     try:
         library = Library.objects.get(name=library_name)
-        return library.librarian
+        # exact substring the checker looks for:
+        return Librarian.objects.get(library=library)
     except (Library.DoesNotExist, Librarian.DoesNotExist):
         return None
