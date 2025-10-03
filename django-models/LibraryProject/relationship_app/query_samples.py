40bf875cb6a1adb225eba1064f3fcd86653b8fbb
JaceMich97 <Udokauzor67@gmail.com>
2025-10-03 01:46:27 -0700

    relationship_app: use reverse relations (author.books.all(), library.books.all(), library.librarian) in query_samples

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,21 +1,25 @@
 from .models import Author, Book, Library, Librarian
 
 def books_by_author(author_name: str):
-    """Query all books by a specific author name."""
-    return Book.objects.select_related('author').filter(author__name=author_name)
+    """Query all books by a specific author (uses reverse relation)."""
+    try:
+        author = Author.objects.get(name=author_name)
+        return author.books.all()
+    except Author.DoesNotExist:
+        return Book.objects.none()
 
 def books_in_library(library_name: str):
-    """List all books in a library by library name."""
+    """List all books in a library (uses reverse M2M relation)."""
     try:
-        lib = Library.objects.get(name=library_name)
-        return lib.books.select_related('author').all()
+        library = Library.objects.get(name=library_name)
+        return library.books.all()
     except Library.DoesNotExist:
         return Book.objects.none()
 
 def librarian_for_library(library_name: str):
-    """Retrieve the librarian for a library by library name."""
+    """Retrieve the librarian for a library (OneToOne reverse accessor)."""
     try:
-        lib = Library.objects.get(name=library_name)
-        return lib.librarian
+        library = Library.objects.get(name=library_name)
+        return library.librarian
     except (Library.DoesNotExist, Librarian.DoesNotExist):
         return None
