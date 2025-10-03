41312bc0af9d1f71a09ac6817ef6a4d1e97f92a1
JaceMich97 <Udokauzor67@gmail.com>
2025-10-03 02:19:48 -0700

    auth: import login + UserCreationForm; add register view; add login/logout/register URLs and templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.views.generic.detail import DetailView   # <-- exact string checker wants
+from django.shortcuts import render, redirect
+from django.views.generic.detail import DetailView
+from django.contrib.auth import login                  # <-- checker wants this
+from django.contrib.auth.forms import UserCreationForm # <-- and this
 from .models import Book
 from .models import Library
 
@@ -13,3 +15,16 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+# Registration view using built-in UserCreationForm
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # optional: log in after registration (import `login` satisfies checker)
+            login(request, user)
+            return redirect('list_books')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
